ed85f2ea2ecbdf4c1c116ea3cac81fa0cab0e69d
Rahul Jagetiya <63360644+clubofcodes@users.noreply.github.com>
2022-02-02 15:11:48 +0530

    React Component render() method Implementation in App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './Assets/lessRes.jpg';
 import './App.css';
+
+// Input data that is passed into the React.component can be accessed by render() method via this.props.
+class HelloMessage extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello {this.props.name}
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='App-flip-box'>
+          <div className='App-flip-box-inner'>
+            <div className='App-flip-box-front'>
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div className='App-flip-box-back'>
+              <div style={{padding: '50px 0'}}>
+                <HelloMessage name="Rahul Jagetia" />
+                <p>Welcome to React World!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
       </header>
     </div>
   );
