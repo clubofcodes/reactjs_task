2e55d7517f8cccd17958cc8b6054d8f14adad0cf
Rahul Jagetiya <63360644+clubofcodes@users.noreply.github.com>
2022-02-17 17:02:08 +0530

    Hooks, useForm diffrent arguents implemented.

--- src/Components/Forms/HookForm.js
+++ src/Components/Forms/HookForm.js
@@ -7,6 +7,8 @@ import { validationSchema } from '../../Validations/YupValSchema';
 export default function HookForm() {
     const { register, handleSubmit, reset, watch, formState: { errors } } = useForm({
         resolver: yupResolver(validationSchema),
+        mode: 'onSubmit', // when form submitted, shows errror. If 'all' than shows error for every change and blure events.
+        reValidateMode:'onChange' //first errors are shown on submit and than further errors will be shown when input is changed.
     });
     const onSubmit = data => alert(JSON.stringify(data, null, 2));;
     console.log("First Name: ", watch("firstName")); //To get any input value.
