43efd04542733d5c5c385bca44b808dfe933df3b
Rahul Jagetiya <63360644+clubofcodes@users.noreply.github.com>
2022-02-22 14:01:54 +0530

    Comments in Callback using ContextAPI

--- src/Components/RestAPIsHttpMethods/ContextCallApiFuncComp.js
+++ src/Components/RestAPIsHttpMethods/ContextCallApiFuncComp.js
@@ -2,4 +2,8 @@ import React, { useContext } from 'react';
 import { UserContext } from './AjaxFetchClassComp';
 import '../../Assets/Css/custom.css';
 
+/**
+ * 
+ * @returns button html code and it's click event calls callback function of parent component.
+ */
 export const CallApiBtn = () => <div><button className='btn btn-outline-primary btn-lg' onClick={useContext(UserContext).callApi}>Call Api</button></div>
